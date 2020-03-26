2a46762f6a7d426a24d56f4f0d71abdbdbe886b7
Mena Bebawy <eng.mena.lorans@gmail.com>
2020-03-26 09:05:59 -0700

    copy rx project

---
+++ Podfile
@@ -0,0 +1,18 @@
+platform :ios, '13.0'
+
+target 'Weather-For-Three-Days-Reactor' do
+  use_frameworks!
+
+  pod 'RxSwift', '~> 5.0'
+  pod 'RxCocoa', '~> 5.0'
+  pod 'ReactorKit', '2.0.1'
+  pod 'RxDataSources', '~> 4.0'
+  pod 'Kingfisher', '~> 5.0'
+  pod 'SwiftLint', '0.39.1'
+  pod 'Endpoints'
+
+  target 'Weather-For-Three-Days-ReactorTests' do
+    inherit! :search_paths
+  end
+
+end
---
+++ Weather-For-Three-Days-ReactorTests/Weather_For_Three_Days_ReactorTests.swift
@@ -0,0 +1,34 @@
+//
+//  Weather_For_Three_Days_ReactorTests.swift
+//  Weather-For-Three-Days-ReactorTests
+//
+//  Created by Mena Bebawy on 3/26/20.
+//  Copyright © 2020 Mena. All rights reserved.
+//
+
+import XCTest
+@testable import Weather_For_Three_Days_Reactor
+
+class Weather_For_Three_Days_ReactorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
